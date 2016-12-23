d1d9d6c657a984aa20a04a01d503861ad5de1a64
Envy <envy_83@mail.ru>
2016-12-23 11:19:43 +0200

    khn menu

--- khn/js/main.js
+++ khn/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-           
+    $('.subheader').hide();   
+    $('.burger').on('click', function() {
+        $('.subheader').slideDown();
+    });
+    $('.menu_close').on('click', function() {
+        $('.subheader').slideUp();
+    });
 });
