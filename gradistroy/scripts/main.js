647d96b01b3d2b8fd6bfd900e979cfeda30a9569
Envy <envy_83@mail.ru>
2017-01-24 10:04:33 +0200

    gradistroy

---
+++ gradistroy/scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    /*input mask*/
+    $("#writeCall").mask("+7(999)999-99-99");
+    
+    
+    /*лайтбокс галереи*/
+    $(document).ready(function() { 
+        $("a.fancyimage").fancybox(); 
+      }); 
+});
