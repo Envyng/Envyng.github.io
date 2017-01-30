0abf08ec35ed77d7ffa8b1c97ee62e2350882cc1
Envy <envy_83@mail.ru>
2017-01-30 14:03:01 +0200

    jskl phpex

--- jp_kn/scripts/main.js
+++ jp_kn/scripts/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+    $('[data-mask="true"]').inputmask({ greedy: false, placeholder: '' });
+    
     
     /*main menu mobile*/
     $('.burger').on('click', function() {
         $('.main_menu ul').toggleClass('hidden-xs');
-        
     });
     
     /*Управление блоком услуг*/
@@ -62,4 +63,77 @@ $(document).ready(function() {
             items: 1
         });
     }
+
+
+    $('body').on('click', '.promotion_video > .youtube-launcher', function() {
+        $(this).hide();
+        var ifr = $(this).closest('.promotion_video').find('iframe');
+        ifr[0].src += '&autoplay=1';
+        ifr.show();
+    });
+
+    var regMail = /^[\w\.\d-_]+@[\w\.\d-_]+\.\w{2,4}$/i;
+
+    $('body').on('submit', '#contactForm', function(e) {
+        e.preventDefault();
+        var cForm = $(this);
+        var cFormData = cForm.serialize();
+
+
+        if($(this).find('input[name="first_name"]').val().length < 2) {
+            $(this).find('input[name="first_name"]').css({'background-color':'rgba(255, 137, 130, 1)'});
+            setTimeout(function(){
+                cForm.find('input[name="first_name"]').removeAttr('style');
+            },500);
+        } else {
+            $(this).find('input[name="first_name"]').removeAttr('style');
+        }
+
+        if($(this).find('input[name="last_name"]').val().length < 2) {
+            $(this).find('input[name="last_name"]').css({'background-color':'rgba(255, 137, 130, 1)'});
+            setTimeout(function(){
+                cForm.find('input[name="last_name"]').removeAttr('style');
+            },500);
+        } else {
+            $(this).find('input[name="last_name"]').removeAttr('style');
+        }
+
+        if($(this).find('input[name="phone"]').val().length < 5) {
+            $(this).find('input[name="phone"]').css({'background-color':'rgba(255, 137, 130, 1)'});
+            setTimeout(function(){
+                cForm.find('input[name="phone"]').removeAttr('style');
+            },500);
+        } else {
+            $(this).find('input[name="phone"]').removeAttr('style');
+        }
+
+        if($(this).find('input[name="email"]').val().match(regMail)) {
+            $(this).find('input[name="email"]').removeAttr('style');
+        } else {
+            $(this).find('input[name="email"]').css({'background-color':'rgba(255, 137, 130, 1)'});
+            setTimeout(function(){
+                cForm.find('input[name="email"]').removeAttr('style');
+            },500);
+        }
+        if(
+            $(this).find('input[name="first_name"]').val().length >= 2 &&
+            $(this).find('input[name="last_name"]').val().length >= 2 &&
+            $(this).find('input[name="phone"]').val().length >= 5 &&
+            $(this).find('input[name="email"]').val().match(regMail)
+        ) {
+            $.ajax({
+                type: "POST",
+                url: "./",
+                data: cFormData,
+                success: function(data) {
+                    if(data == 'ok') {
+                        $('#thanksModal').modal('show');
+                    }
+                },
+                error: function() {
+                }
+            });
+        }
+    });
+
 });
